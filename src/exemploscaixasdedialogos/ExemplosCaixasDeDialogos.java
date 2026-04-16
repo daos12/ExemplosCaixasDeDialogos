fa81a50d707fe4eadb63caf9544ceede0636c7d5
daos12 <daos_ptc@hotmail.com>
2026-04-16 20:21:54 -0300

    showMessageDialog

--- src/exemploscaixasdedialogos/ExemplosCaixasDeDialogos.java
+++ src/exemploscaixasdedialogos/ExemplosCaixasDeDialogos.java
@@ -1,7 +1,13 @@
 package exemploscaixasdedialogos;
 
+import javax.swing.JOptionPane;
+
 public class ExemplosCaixasDeDialogos {
     public static void main(String[] args) {
+       
+        //showMessageDialog saída de dados
+        JOptionPane.showMessageDialog(null, "Bem-vindo ao sistema!");
         
+
     }
 }
