13ad284e655f4d343f5513898eaf28854cf1f869
daos12 <daos_ptc@hotmail.com>
2026-04-16 21:53:36 -0300

    showOptionDialog

--- src/exemploscaixasdedialogos/ExemplosCaixasDeDialogos.java
+++ src/exemploscaixasdedialogos/ExemplosCaixasDeDialogos.java
@@ -3,25 +3,39 @@ package exemploscaixasdedialogos;
 import javax.swing.JOptionPane;
 
 public class ExemplosCaixasDeDialogos {
+
     public static void main(String[] args) {
-//        System.out.println("");
-//        //showMessageDialog saída de dados
-//        JOptionPane.showMessageDialog(null, "Bem-vindo ao sistema!");
-//        
-//        //showComfirmDialog
-//        int resposta = JOptionPane.showConfirmDialog(null, "Deseja sair?");
-//        
-//        if(resposta == JOptionPane.YES_NO_OPTION){
-//            JOptionPane.showMessageDialog(null, "Saindo do sistema.");
-//        }else{
-//            JOptionPane.showMessageDialog(null, "Continuando no sistema.");
-//        }
-        
+        System.out.println("");
+        //showMessageDialog saída de dados
+        JOptionPane.showMessageDialog(null, "Bem-vindo ao sistema!");
+
+        //showComfirmDialog
+        int resposta = JOptionPane.showConfirmDialog(null, "Deseja sair?");
+
+        if (resposta == JOptionPane.YES_NO_OPTION) {
+            JOptionPane.showMessageDialog(null, "Saindo do sistema.");
+        } else {
+            JOptionPane.showMessageDialog(null, "Continuando no sistema.");
+        }
+
         //showInputDialog
         String nome = JOptionPane.showInputDialog("Digite seu nome:");
-        
+
         JOptionPane.showMessageDialog(null, "Nome do usuario: " + nome);
-        
-        
+
+        //showOptionDialog
+        String[] opcoes = {"Sim", "Não", "Batata"};
+
+        int escolha = JOptionPane.showOptionDialog(
+                null,                               //sempre null
+                "Continuar?",                       //Mensagem para o usuario
+                "Escolha opções",                   //Titulo da caixa
+                JOptionPane.DEFAULT_OPTION,         //Botões desabilitados
+                JOptionPane.INFORMATION_MESSAGE,    //Icones
+                null,                               //Sempre null
+                opcoes,                             //Vetor 
+                opcoes[0]);                         //Inicio do vetor
+
     }
+
 }
