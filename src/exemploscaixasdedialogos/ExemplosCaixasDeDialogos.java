060dfb5f4d4ce3e6b6ba413cd239619c51277ca7
daos12 <daos_ptc@hotmail.com>
2026-04-16 21:33:56 -0300

    showInputDialog

--- src/exemploscaixasdedialogos/ExemplosCaixasDeDialogos.java
+++ src/exemploscaixasdedialogos/ExemplosCaixasDeDialogos.java
@@ -4,18 +4,23 @@ import javax.swing.JOptionPane;
 
 public class ExemplosCaixasDeDialogos {
     public static void main(String[] args) {
-        System.out.println("");
-        //showMessageDialog saída de dados
-        JOptionPane.showMessageDialog(null, "Bem-vindo ao sistema!");
+//        System.out.println("");
+//        //showMessageDialog saída de dados
+//        JOptionPane.showMessageDialog(null, "Bem-vindo ao sistema!");
+//        
+//        //showComfirmDialog
+//        int resposta = JOptionPane.showConfirmDialog(null, "Deseja sair?");
+//        
+//        if(resposta == JOptionPane.YES_NO_OPTION){
+//            JOptionPane.showMessageDialog(null, "Saindo do sistema.");
+//        }else{
+//            JOptionPane.showMessageDialog(null, "Continuando no sistema.");
+//        }
         
-        //showComfirmDialog
-        int resposta = JOptionPane.showConfirmDialog(null, "Deseja sair?");
+        //showInputDialog
+        String nome = JOptionPane.showInputDialog("Digite seu nome:");
         
-        if(resposta == JOptionPane.YES_NO_OPTION){
-            JOptionPane.showMessageDialog(null, "Saindo do sistema.");
-        }else{
-            JOptionPane.showMessageDialog(null, "Continuando no sistema.");
-        }
+        JOptionPane.showMessageDialog(null, "Nome do usuario: " + nome);
         
         
     }
